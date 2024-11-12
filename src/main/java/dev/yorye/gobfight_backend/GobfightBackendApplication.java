afdc89d1c8956e602b243134bb74cbf7555bf754
Yorye <yorye.dev@gmail.com>
2024-11-12 20:32:32 +0100

    init de backend

---
+++ src/main/java/dev/yorye/gobfight_backend/GobfightBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.yorye.gobfight_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GobfightBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GobfightBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/yorye/gobfight_backend/GobfightBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yorye.gobfight_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GobfightBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
