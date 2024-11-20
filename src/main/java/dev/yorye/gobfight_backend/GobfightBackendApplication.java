cf4c4a9ef59d4eb0405ba1e9349cb0d3a655ec45
Yorye <yorye.dev@gmail.com>
2024-11-20 21:32:12 +0100

    Driver conector bdd Postgresql

--- src/main/java/dev/yorye/gobfight_backend/GobfightBackendApplication.java
+++ src/main/java/dev/yorye/gobfight_backend/GobfightBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GobfightBackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GobfightBackendApplication.class, args);
 	}
 
---
+++ src/main/java/dev/yorye/gobfight_backend/controllers/authControllers.java
@@ -0,0 +1,8 @@
+package dev.yorye.gobfight_backend.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public class authControllers {
+
+}
