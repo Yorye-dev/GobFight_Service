be8726dbb1957c782356ff450217cee26c89689b
yorye <yorye.dev@gmail.com>
2025-01-09 19:49:02 +0100

    feat(persistence): configurar conexión con PostgreSQL y entidad User

--- src/main/java/dev/yorye/gobfight_backend/config/DatabaseConfig.java
+++ src/main/java/dev/yorye/gobfight_backend/config/DatabaseConfig.java
@@ -2,9 +2,11 @@ package dev.yorye.gobfight_backend.config;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
+@Configuration
 public class DatabaseConfig {
 
     @Value("${spring.datasource.url}")
