83ab9bb30a2bea418c39659ff5e4716374e6e29d
Yorye-dev <jorge.abrodes@gmail.com>
2025-01-09 21:13:50 +0100

    feat(auth): create defaullt IMPL

--- src/main/java/dev/yorye/gobfight_backend/user/service/UserServiceImpl.java
+++ src/main/java/dev/yorye/gobfight_backend/user/service/UserServiceImpl.java
@@ -1,4 +1,32 @@
 package dev.yorye.gobfight_backend.user.service;
 
+import dev.yorye.gobfight_backend.user.dto.UserDto;
+
+import java.util.List;
+
 public class UserServiceImpl implements UserService{
+    @Override
+    public UserDto getUserById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
+        return List.of();
+    }
+
+    @Override
+    public void createNewUser(UserDto user) {
+
+    }
+
+    @Override
+    public void updateUser(Long id, UserDto userDto) {
+
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+
+    }
 }
